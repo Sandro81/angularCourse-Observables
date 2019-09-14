c6f23c833f73af9c83f5f59f020a0b3c298b5819
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-14 10:15:42 +0100

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,13 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe( didActivate => {
+   this.activatedSub =  this.userService.activatedEmitter.subscribe( didActivate => {
       this.userActivated = didActivate;
       });
   }
 
-
   ngOnDestroy(): void {
-
+    this.activatedSub.unsubscribe();
   }
 
 
